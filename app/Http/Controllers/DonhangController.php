c82726c63bfd21de6d92123e59551c58a6f22534
thanhhoi123 <87430163+thanhhoi123@users.noreply.github.com>
2021-12-12 21:35:20 +0700

    Update Router
    Update Router and update version

--- app/Http/Controllers/DonhangController.php
+++ app/Http/Controllers/DonhangController.php
@@ -18,6 +18,11 @@ class DonhangController extends Controller
         $donhang = Donhang::where('user_id', $id)->get();
         return $donhang;
     }
+    public function index2(Request $request)
+    {
+        $donhangs = Donhang::where('user_id', $request->user_id)->get();
+        return $donhangs;
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -44,13 +49,72 @@ class DonhangController extends Controller
         ]);
         foreach ($request->sanpham as $sanpham)
         {
-            $product = SanPham::find($sanpham['id'])->first();
+            $product = SanPham::where('id',$request->IDSanPham)->first();
+            if($product->amount<$request->SLSanPham)
+            {
+                return "Not Enough Available";
+            }
             $donhang->sanphams()->save($product, ['amount' => $sanpham['amount']]);
-            $product->amount = $product->amount - $sanpham['amount'];
+            $amount = $product->amount - $request->SLSanPham;
+            SanPham::where('id',$request->IDSanPham)->update(['amount'=>$amount]);
         }
         return $donhang;
     }
-
+    public function store1SP($id, Request $request)
+    {
+        $donhang = Donhang::create([
+            'user_id' => $id,
+            'Address' => $request->address,
+            'Accept' => $request->accept
+        ]);
+        $product = SanPham::where('id',$request->IDSanPham)->first();
+        if($product->amount<$request->SLSanPham)
+        {
+            return "Not Enough Available";
+        }
+        $donhang->sanphams()->save($product, ['amount' => $request->SLSanPham]);
+        $amount = $product->amount - $request->SLSanPham;
+        SanPham::where('id',$request->IDSanPham)->update(['amount'=>$amount]);
+        return $donhang;
+    }
+    public function store2($id, Request $request)
+    {
+        $donhang = Donhang::create([
+            'user_id' => $id,
+            'Address' => $request->address,
+            'Accept' => $request->accept
+        ]);
+        $len = count($request->IDSanPham);
+        for($i=0;$i<$len;$i++)
+        {
+            $product = SanPham::where('id',$request->IDSanPham[$i])->first();
+            if($product->amount<$request->SLSanPham)
+            {
+                return "Not Enough Available";
+            }
+            $donhang->sanphams()->save($product, ['amount'=> $request->SLSanPham[$i]]);
+            $amount = $product->amount - $request->SLSanPham;
+            SanPham::where('id',$request->IDSanPham)->update(['amount'=>$amount]);
+        }
+        return $donhang;
+    }
+    public function store1SP2(Request $request)
+    {
+        $donhang = Donhang::create([
+            'user_id' => $request->user_id,
+            'Address' => $request->address,
+            'Accept' => $request->accept
+        ]);
+        $product = SanPham::where('id',$request->IDSanPham)->first();
+        if($product->amount<$request->SLSanPham)
+        {
+            return "Not Enough Available";
+        }
+        $donhang->sanphams()->save($product, ['amount' => $request->SLSanPham]);
+        $product->amount = (int)$product->amount - (int)$request->SLSanPham;
+        $product->save();
+        return $donhang;
+    }
     /**
      * Display the specified resource.
      *
@@ -67,6 +131,34 @@ class DonhangController extends Controller
         }
         return $sanphams;
     }
+    public function show2($id)
+    {
+        $donhang = Donhang::where('id', $id)->first();
+        $sanphams=[];
+        foreach ($donhang->sanphams as $sanpham)
+        {
+            $sanphams[] =$sanpham;
+        }
+        return ["Address"=>$donhang->Address,"Accept"=> $donhang->Accept,$sanphams];
+    }
+    public function show3(Request $request)
+    {
+        $donhang = Donhang::where('id', $request->IDDH)->first();
+        $sanphams=[];
+        foreach ($donhang->sanphams as $sanpham)
+        {
+            $sanphams[] =$sanpham;
+        }
+        return ["Address"=>$donhang->Address,"Accept"=> $donhang->Accept,
+                "id"=>$sanphams[0]->id,
+                "created_at"=>$sanphams[0]->created_at,
+                "updated_at"=>$sanphams[0]->updated_at,
+                "name"=>$sanphams[0]->name,
+                "amount"=>$sanphams[0]->amount,
+                "Price"=>$sanphams[0]->Price,
+                "description"=>$sanphams[0]->description,
+                "img_link"=>$sanphams[0]->img_link];
+    }
 
     /**
      * Show the form for editing the specified resource.
@@ -78,7 +170,22 @@ class DonhangController extends Controller
     {
         //
     }
+    public function findUnAccept($id)
+    {
+        $donhang = Donhang::where('user_id',$id)->where('Accept', 0)->first();
+        if($donhang === null)
+        {
 
+            return ["Message"=>"create new Donhang"];
+        }
+        $sanphams=[];
+        foreach ($donhang->sanphams as $sanpham)
+        {
+            $sanphams[] =$sanpham;
+
+        }
+        return ["Address"=>$donhang->Address,"Accept"=> $donhang->Accept,"SanPham:"=>$donhang->sanphams];
+    }
     /**
      * Update the specified resource in storage.
      *
@@ -91,15 +198,15 @@ class DonhangController extends Controller
         try
         {
             $donhang = Donhang::where('id', $id)->first();
-            if($request->has('address'))
+            if($request->has('Address'))
             {
-                $donhang->update(['Address'=> $request->get('address')]);
+                $donhang->update(['Address'=> $request->get('Address')]);
             }
-            if($request->has('accept'))
+            if($request->has('Accept'))
             {
-                $donhang->update(['Accept'=> $request->get('accept')]);
+                $donhang->update(['Accept'=> $request->get('Accept')]);
             }
-            if($request->has('sanpham'))
+            if($request->has('Sanpham'))
             {
                 $donhang->sanphams()->detach();
                 foreach ($request->sanpham as $sanpham)
